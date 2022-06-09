bc2e52780f02f10878900e8870d91bf6cb930b83
Oleksii Petrychenko <oleksii_petrychenko@epam.com>
2022-06-09 10:48:30 +0300

    2 - Styles and markup are implemented 3 - Used stateless/stateful approach 4 - Used event handlers 5 - Use lifecycle methods or useEffect hook (discuss with mentor)

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,7 +1,20 @@
-import React from "react";
-import './search.scss'
+import React, { useState, useEffect } from "react";
+import "./search.scss";
 
 const Search = () => {
+  const [movie, setMovie] = useState("");
+
+  useEffect(() => {
+    if (movie.length > 2) {
+      console.log("ищем", movie);
+    }
+  }, [movie]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(movie, "отправлена форма");
+  };
+
   return (
     <div className="search">
       <div className="search-background"></div>
@@ -10,13 +23,17 @@ const Search = () => {
         <button className="search-head-add">+ add movie</button>
       </div>
       <h1 className="search-heading">FIND YOUR MOViE</h1>
-      <form className="search-form">
+      <form onSubmit={handleSubmit} className="search-form">
         <input
           className="search-form-input"
           type="text"
           placeholder="What do you want to watch?"
+          value={movie}
+          onChange={(e) => setMovie(e.target.value)}
         />
-        <button className="search-form-button">search</button>
+        <button type="submit" className="search-form-button">
+          search
+        </button>
       </form>
     </div>
   );
